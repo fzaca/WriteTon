ff4bdb9f5cd345a8477173a29c7cf27752b8a170
Zacarias <zaca03zaca@gmail.com>
2024-04-05 22:01:35 -0300

    Add database creation functionality and error handling

--- src/database.rs
+++ src/database.rs
@@ -11,3 +11,19 @@ pub fn get_conn() -> Result<Connection> {
     ))?;
     Ok(conn)
 }
+
+pub fn ensure_database_created() -> Result<()> {
+    match get_conn() {
+        Ok(conn) => {
+            conn.execute(
+                "CREATE TABLE note (
+                    id INTEGER PRIMARY KEY,
+                    content TEXT
+                )",
+                (),
+            )?;
+            Ok(())
+        }
+        Err(err) => Err(err),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,11 @@ mod utils;
 fn main() {
     let args = Cli::parse();
 
+    if let Err(err) = database::ensure_database_created() {
+        println!("Error creating the database: {err}");
+        std::process::exit(1);
+    }
+
     match args.command {
         Commands::New { content } => commands::new::exec(content),
     }
